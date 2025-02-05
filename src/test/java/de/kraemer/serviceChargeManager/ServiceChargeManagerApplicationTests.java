6a8d9403700a3e9794a2e4493a0306437f4a6f12
Jan Kraemer <jan.kraemer.22@gmail.com>
2025-02-05 14:27:29 +0100

    test: spring modulith test
    Added test for spring modulith to comply with rules and generate PlantUML diagrams for all modules.
    
    Signed-off-by: Jan Kraemer <jan.kraemer.22@gmail.com>

--- src/test/java/de/kraemer/serviceChargeManager/ServiceChargeManagerApplicationTests.java
+++ src/test/java/de/kraemer/serviceChargeManager/ServiceChargeManagerApplicationTests.java
@@ -3,13 +3,22 @@ package de.kraemer.serviceChargeManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ServiceChargeManagerApplicationTests {
+  private final ApplicationModules modules =
+      ApplicationModules.of(ServiceChargeManagerApplication.class);
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {}
 
+  @Test
+  void compliesWithModulithRules() {
+    this.modules.verify();
+
+    new Documenter(modules).writeModulesAsPlantUml().writeIndividualModulesAsPlantUml();
+  }
 }
