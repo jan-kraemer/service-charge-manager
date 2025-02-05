140fc2d28e262224677333d742c05fe61d5bdb06
Jan Kraemer <jan.kraemer.22@gmail.com>
2025-02-05 14:38:57 +0100

    fix(resident): add resident database table
    Signed-off-by: Jan Kraemer <jan.kraemer.22@gmail.com>

---
+++ src/main/resources/db/migration/V2__create_resident_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS resident
+(
+    id
+    UUID
+    PRIMARY
+    KEY
+    NOT
+    NULL,
+    first_name
+    TEXT,
+    sure_name
+    TEXT,
+    email
+    TEXT,
+    phone_number
+    TEXT
+);
