0ed6779ceca0190028dd0e32cf1acef2692d6e24
Jan Kraemer <jan.kraemer.22@gmail.com>
2025-02-05 14:22:27 +0100

    style: line format
    Signed-off-by: Jan Kraemer <jan.kraemer.22@gmail.com>

--- src/main/java/de/kraemer/serviceChargeManager/ServiceChargeManagerApplication.java
+++ src/main/java/de/kraemer/serviceChargeManager/ServiceChargeManagerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceChargeManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceChargeManagerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceChargeManagerApplication.class, args);
+  }
 }
